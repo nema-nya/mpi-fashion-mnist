fde0f179fa17e96ea241d0d4cc83db1458c0c20c
nema-nya <nemanjagabor2001@gmail.com>
2026-03-07 19:13:51 +0100

    added test set

--- mpi_main.c
+++ mpi_main.c
@@ -21,49 +21,23 @@ int main(int argc, char** argv) {
     Dataset d;
     RETURN_IF_ERROR(
         dataset_load_bin("data/train-labels.bin", "data/train-data.bin", &d));
-    float frac_to_take = 0.01;
-    int n_of_data_to_take = 60000 * frac_to_take;
 
-    Tensor* new_x =
-        tensor_alloc(shapeN(3, n_of_data_to_take, 1, 784), DTYPE_FLOAT32);
-    Tensor* new_y = tensor_alloc(shapeN(1, n_of_data_to_take), DTYPE_UINT8);
+    Dataset d_test;
+    RETURN_IF_ERROR(dataset_load_bin("data/test-labels.bin",
+                                     "data/test-data.bin", &d_test));
 
     reshape(d.x, shapeN(3, 60000, 1, 784));
-    RETURN_IF_ERROR(tensor_slice(d.x, new_x, 0, 0, n_of_data_to_take));
-    RETURN_IF_ERROR(tensor_slice(d.y, new_y, 0, 0, n_of_data_to_take));
-    free(d.x);
-    free(d.y);
-    d.x = new_x;
-    d.y = new_y;
-
-    Tensor t1 = {0};
-    Tensor t2 = {0};
-    Shape s_2 = shape3(3, 1, 5);
-    Shape s_1 = shapeN(4, 1, 3, 2, 5);
-    if (tensor_init(&t1, s_1, DTYPE_FLOAT32)) {
-        printf("failed to allocate tensors 1\n");
-        tensor_free(&t1);
-        tensor_free(&t2);
-        dataset_free(&d);
-        return 1;
-    }
-    if (tensor_init(&t2, s_2, DTYPE_FLOAT32)) {
-        printf("failed to allocate tensors 2\n");
-        tensor_free(&t1);
-        tensor_free(&t2);
-        dataset_free(&d);
-        return 1;
-    }
+    reshape(d_test.x, shapeN(3, 10000, 1, 784));
 
     float acc = 0.0f;
     float loss = 0.0f;
     size_t t = 0;
-    size_t epochs = 5;
+    size_t epochs = 1;
     float lr = 0.001f;
     float beta1 = 0.9f;
     float beta2 = 0.999f;
     float eps = 1e-08f;
-    int batch_size = 8;
+    int batch_size = 64;
 
     Tensor* layer1_weight = tensor_alloc(shapeN(2, 784, 256), DTYPE_FLOAT32);
     Tensor* layer1_bias = tensor_alloc(shapeN(1, 256), DTYPE_FLOAT32);
@@ -71,7 +45,7 @@ int main(int argc, char** argv) {
     Tensor* layer2_bias = tensor_alloc(shapeN(1, 10), DTYPE_FLOAT32);
 
     RETURN_IF_ERROR(reshape(layer1_weight, shapeN(3, 1, 784, 256)));
-    RETURN_IF_ERROR(reshape(d.x, shapeN(3, n_of_data_to_take, 1, 784)));
+    RETURN_IF_ERROR(reshape(d.x, shapeN(3, d.n, 1, 784)));
     RETURN_IF_ERROR(reshape(layer2_weight, shapeN(3, 1, 256, 10)));
 
     RNG r;
@@ -135,8 +109,7 @@ int main(int argc, char** argv) {
     Tensor* batch_y = tensor_alloc(d_y_shape, DTYPE_UINT8);
     for (size_t ep = 0; ep < epochs; ++ep) {
         dataset_rand_perm(d.x, d.y, &r);
-        for (size_t batch = 0;
-             batch < n_of_data_to_take - batch_size * world_size;
+        for (size_t batch = 0; batch < d.n - batch_size * world_size;
              batch += batch_size * world_size) {
             RETURN_IF_ERROR(
                 tensor_slice(d.x, batch_x, 0, batch + world_rank * batch_size,
@@ -173,8 +146,8 @@ int main(int argc, char** argv) {
 
             RETURN_IF_ERROR(reshape(hidden_2, shapeN(2, batch_size, 10)));
             RETURN_IF_ERROR(cross_entropy(hidden_2, batch_y, &loss));
-            printf("%d epoch %zu loss = %.5f acc = %.5f\n", world_rank, ep,
-                   loss, acc);
+            printf("epoch %zu loss = %.5f acc = %.5f %d/%d\n", ep, loss, acc,
+                   batch, d.n);
             ////////////////////////////////////////////////////////////////////////////////////////////
             RETURN_IF_ERROR(
                 cross_entropy_backward(hidden_2, batch_y, hidden_2_grad));
@@ -228,6 +201,33 @@ int main(int argc, char** argv) {
                                       layer2_bias_grad, layer2_bias,
                                       layer2_bias_m, layer2_bias_v));
         }
+    }
+    for (size_t batch = 0; batch < d_test.n - batch_size; batch += batch_size) {
+        RETURN_IF_ERROR(
+            tensor_slice(d_test.x, batch_x, 0, batch, batch + batch_size));
+        RETURN_IF_ERROR(
+            tensor_slice(d_test.y, batch_y, 0, batch, batch + batch_size));
+        tensor_fill_float(hidden_1, 0.0f);
+        tensor_fill_float(hidden_2, 0.0f);
+
+        RETURN_IF_ERROR(bmm(hidden_1, batch_x, layer1_weight, false, false));
+        RETURN_IF_ERROR(tensor_add(hidden_1, layer1_bias));
+
+        RETURN_IF_ERROR(tensor_tanh(hidden_1));
+
+        RETURN_IF_ERROR(bmm(hidden_2, hidden_1, layer2_weight, false, false));
+        RETURN_IF_ERROR(tensor_add(hidden_2, layer2_bias));
+
+        RETURN_IF_ERROR(tensor_argmax(hidden_2, argmax_out));
+        RETURN_IF_ERROR(reshape(argmax_out, shapeN(1, batch_size)));
+        RETURN_IF_ERROR(accuracy(argmax_out, batch_y, &acc));
+        RETURN_IF_ERROR(reshape(argmax_out, shapeN(2, batch_size, 1)));
+
+        RETURN_IF_ERROR(reshape(hidden_2, shapeN(2, batch_size, 10)));
+        RETURN_IF_ERROR(cross_entropy(hidden_2, batch_y, &loss));
+        printf("\rloss = %.5f acc = %.5f %d/%d\n", loss, acc, batch, d_test.n);
+
+        RETURN_IF_ERROR(reshape(hidden_2, shapeN(3, batch_size, 1, 10)));
     }
     MPI_Finalize();
     return 0;
